f00ab5f3ae2a4572f89584804c630e612a28047a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MVC_app/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using DevExpress.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace MvcGridView_example.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
---
+++ CS/MVC_app/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_app.MvcApplication" Language="C#" %>
